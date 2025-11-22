ec4f9e5cd967b5110e07b57bf9657fe52aa9f234
Mishan Aliev <66633214+thecrazymage@users.noreply.github.com>
2025-11-22 18:25:01 +0300

    fix script

--- scripts/objaverse_data_processing.sh
+++ scripts/objaverse_data_processing.sh
@@ -3,4 +3,4 @@ module load Python CUDA/11.7 gnu10
 source deactivate
 conda activate t2t
 
-python ./objaverse_eval/utils/download_objaverse_subset.py
+python ./objaverse_eval/objaverse_data_utils/download_objaverse_subset.py
