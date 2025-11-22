863b1cdbb698fb1529b888e02ce45440e029e99c
Mishan Aliev <66633214+thecrazymage@users.noreply.github.com>
2025-11-22 17:52:01 +0300

    add objaverse processing files from text2tex

---
+++ objaverse_eval/utils/parameterize_mesh.py
@@ -0,0 +1,26 @@
+# -----------------------------------------------------------------------------
+# Copied from Text2Tex (https://github.com/daveredrum/Text2Tex)
+# Copyright (c) 2023 Dave Zhenyu Chen et al.
+# Licensed under CC BY-NC-SA 3.0.
+# -----------------------------------------------------------------------------
+
+import xatlas
+import trimesh
+import argparse
+
+def parameterize_mesh(input_path, output_path):
+    # parameterize the mesh
+    mesh = trimesh.load_mesh(input_path, force='mesh')
+    vertices, faces = mesh.vertices, mesh.faces
+
+    vmapping, indices, uvs = xatlas.parametrize(vertices, faces)
+    xatlas.export(str(output_path), vertices[vmapping], indices, uvs)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_path", type=str, required=True)
+    parser.add_argument("--output_path", type=str, required=True)
+    args = parser.parse_args()
+
+    parameterize_mesh(args.input_path, args.output_path)
+    
