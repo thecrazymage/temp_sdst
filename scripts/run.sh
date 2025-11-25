418a5922ae2208c07d9223060d1afd927872a787
Mishan Aliev <66633214+thecrazymage@users.noreply.github.com>
2025-11-25 20:24:06 +0300

    add one sh example

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+
 module load Python CUDA/11.7 gnu10
 source deactivate
 conda activate sdst
 
-MESH_PATH=
-
-PROMPT="A wooden toy cow, handcrafted, child-friendly, rounded body, short legs, small horns, simple snout, light maple with visible grain, warm varnish."
+PROMPT="A cow in a spider-man suit"
 
 python main.py \
     --mesh_location "data/spot.obj" \
     --prompt "$PROMPT" \
     --num_steps_i 500 \
     --num_steps_ii 1000 \
+    --guidance_scale_i 20 \
+    --guidance_scale_ii 15 \
     --use_dir_embeddings
