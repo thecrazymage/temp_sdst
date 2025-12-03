a4c5100eed5922a22bbb00c03b99daafd693aae9
Mishan Aliev <66633214+thecrazymage@users.noreply.github.com>
2025-12-04 00:40:13 +0300

    delete setup_env file

--- scripts/setup_environment.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-module load Python CUDA/12.4 gnu10
-conda env create -f environment.yml -n castex
-conda activate castex
-pip install "git+https://github.com/facebookresearch/pytorch3d.git@V0.7.8" --no-build-isolation
