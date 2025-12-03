6b4f2c0637ce4cb810aecdfbf64c4190592a7740
Mishan Aliev <66633214+thecrazymage@users.noreply.github.com>
2025-12-04 00:37:28 +0300

    update script

--- scripts/objaverse_data_processing.sh
+++ scripts/objaverse_data_processing.sh
@@ -4,4 +4,4 @@ module load Python CUDA/12.4 gnu10
 source deactivate
 conda activate castex
 
-python ./objaverse_eval/objaverse_data_utils/download_objaverse_subset.py
+python objaverse_eval/objaverse_data_utils/download_objaverse_subset.py
