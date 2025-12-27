98e76f467186daf72aa3cb634b12d356b8940a09
Mishan Aliev <66633214+thecrazymage@users.noreply.github.com>
2025-12-27 17:51:34 +0300

    update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,13 +4,16 @@ module load Python CUDA/12.4 gnu10
 source deactivate
 conda activate castex
 
-PROMPT="A cow in a spider-man suit"
+PROMPT="A cow in a suit"
+# PROMPT="A cow wearing a chef outfit"
+# PROMPT="A cow in a Wonder Woman suit"
+# PROMPT="A cow dressed as a doctor in a white coat"
 
 python main.py \
     --mesh_location "assets/spot.obj" \
-    --prompt "$PROMPT" \
+    --prompt "$CURRENT_PROMPT" \
     --num_steps_i 500 \
     --num_steps_ii 1000 \
-    --guidance_scale_i 20 \
-    --guidance_scale_ii 15 \
+    --guidance_scale_i 25 \
+    --guidance_scale_ii 20 \
     --use_dir_embeddings
